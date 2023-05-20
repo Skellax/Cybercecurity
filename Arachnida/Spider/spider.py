d0ef370cc87808313d45fc8133464ac00792945f
Maxime Fuhrmann <mfuhrman@k1r2p14.42mulhouse.fr>
2023-05-20 09:07:38 +0200

    add scorpion

--- Arachnida/Spider/spider.py
+++ Arachnida/Spider/spider.py
@@ -1,10 +1,23 @@
 #!/usr/bin/env python3
 
-import requests 
+import requests
+import shutil
 import os
 from bs4 import BeautifulSoup
+import argparse
 
-def download_Img(url, directory):
+
+
+class Colors:
+    BLUE = '\033[95m'
+    GREEN = '\033[92m'
+    RED = '\033[93m'
+    END = '\033[0m'
+
+
+#Fonction permettant de récuperer les images via les balises srcs des images 
+
+def download_img(url, directory):
     response = requests.get(url)
     content = response.content
 
@@ -12,27 +25,61 @@ def download_Img(url, directory):
 
     with open(file_name, "wb") as file:
         file.write(content)
-        print("Donwload img :", file_name)
+    print("Downloaded image:", file_name)
 
-url_page = "https://www.mb-riclin.com/"
 
-os.mkdir('data')
+#Fonction permettant de recuperer les differents liens pour la récursive
 
-repertory = "data"
+def change_url(url_base, url, level, arg_level, directory):
+    response = requests.get(url)
+    if response.status_code == 200:
+        content_page = response.content
+    else:
+        print(f"{Colors.RED}Error: Couldn't open this URL{Colors.END}")
+        exit()
 
-response = requests.get(url_page)
-if (response.status_code == 200):
-    content_page = response.content
+    soup = BeautifulSoup(content_page, 'html.parser')
+
+    balise_imgs = soup.find_all('img')
+
+    for balise_img in balise_imgs:
+        url_img = balise_img["src"]
+        if url_img.find("wordpress") != -1:
+            url_img = url_img.replace("wordpress", "www")
+        if url_img.endswith(('jpeg', 'jpg', 'gif', 'png', 'bmp')):
+            print(f"{Colors.BLUE}---Level: {level}  {Colors.END}")
+            download_img(url_img, directory)
+
+    if level < arg_level:
+        balise_a = soup.find_all('a')
+
+        for balise_href in balise_a:
+            url_href = balise_href.get("href")
+            if url_href and url_href.startswith(url_base):
+                change_url(url_base, url_href, level + 1, arg_level, directory)
+
+
+#Parsing des arugments
+
+parser = argparse.ArgumentParser()
+parser.add_argument('url', help="the URL of the website")
+parser.add_argument('-r', '--recursive', action='store_true')
+parser.add_argument('-l', '--level', type=int, default=5, choices=range(1, 5), help='the level of depth')
+parser.add_argument('-p', '--path', default='./data', help='the name of the directory')
+args = parser.parse_args()
+level = 1
+
+#Creation du nom du dossier si il existe on le suprimme avec son contenu puis on le recréer
+
+if os.path.exists(args.path):
+    shutil.rmtree(args.path)
+
+if not os.path.exists(args.path):
+    os.mkdir(args.path)
+
+if not args.recursive:
+    change_url(args.url, args.url, level, 1, args.path)
 else:
-    print("Error couln't open this url")
-    exit()
+    change_url(args.url, args.url, level, args.level, args.path)
 
-soup = BeautifulSoup(content_page, 'html.parser')
-
-balise_imgs = soup.find_all('img')
-
-for balise_img in balise_imgs:
-    url_img = balise_img["src"]
-    download_Img(url_img, repertory)
-
-
+print(f"{Colors.GREEN}Operation successful!{Colors.END}")
